62feea7b813ea6e30b5963aae68cc3712487facd
huieunkim-dev <huieunkim.dev@gmail.com>
2026-04-02 20:59:23 +0900

    feat: add About component

--- Week2/huieunKim/src/App.jsx
+++ Week2/huieunKim/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,9 @@ function App() {
   return (
     <>
       <Header></Header>
+      <main>
+        <About></About>
+      </main>
     </>
 
   );
