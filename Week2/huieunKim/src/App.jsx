eeee5e96dac2308873d1bff0e9eba9d347ecea21
huieunkim-dev <huieunkim.dev@gmail.com>
2026-04-02 21:04:17 +0900

    feat: add Hobby component(using Props)

--- Week2/huieunKim/src/App.jsx
+++ Week2/huieunKim/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header';
 import About from './components/About';
+import Hobby from './components/Hobby';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <main>
         <About></About>
+        <Hobby></Hobby>
       </main>
     </>
 
